41987f790e141b7fe9a844568ead5fbf707879d6
RaenonX <raenonx0710@gmail.com>
2023-12-09 14:50:01 -0600

    Added final evolution preview in team maker #559
    closes #559

--- src/ui/team/maker/calc/getMaxPotential.ts
+++ src/ui/team/maker/calc/getMaxPotential.ts
@@ -1,7 +1,9 @@
 import {defaultSeedUsage} from '@/const/game/seed';
+import {PokemonFinalEvolutionInfo} from '@/types/game/pokemon';
 import {teamMakerMaxMemberCount, teamMakerProductionPeriod} from '@/ui/team/maker/calc/const';
 import {TeamMakerInputCalculated, TeamMakerRateAtMaxPotentialData} from '@/ui/team/maker/calc/type';
 import {GetTeamMakerCalcPrepOpts} from '@/ui/team/maker/hook/type';
+import {getPokemonFinalEvolutionIds} from '@/utils/game/pokemon';
 import {getEffectiveIngredientProductions} from '@/utils/game/producing/ingredient/multi';
 import {getPokemonProducingRateSingle} from '@/utils/game/producing/main/single';
 import {GetPokemonProducingRateOpts} from '@/utils/game/producing/main/type';
@@ -28,19 +30,41 @@ export const getTeamMakerRateAtMaxPotential = ({
   calculatedInput,
 }: GetTeamMakerRateAtMaxPotentialOpts): TeamMakerRateAtMaxPotentialData[] => {
   return pokeboxList
-    .map((pokeInBox) => {
-      const pokemon = pokedexMap[pokeInBox.pokemon];
+    .flatMap((pokeInBox) => {
+      if (!input.previewFinalEvolution) {
+        return [{
+          evolutionInfo: {
+            id: pokeInBox.pokemon,
+            evolutionCount: pokeInBox.evolutionCount,
+          } satisfies PokemonFinalEvolutionInfo,
+          pokeInBox,
+        }];
+      }
+
+      return getPokemonFinalEvolutionIds({
+        pokemonId: pokeInBox.pokemon,
+        pokedex: pokedexMap,
+        evolutionCount: pokeInBox.evolutionCount,
+      }).map((evolutionInfo) => ({
+        evolutionInfo,
+        pokeInBox,
+      }));
+    })
+    .map(({evolutionInfo, pokeInBox}) => {
+      const {id, evolutionCount} = evolutionInfo;
+      const pokemon = pokedexMap[id];
 
       if (!pokemon) {
         return null;
       }
 
+      const level = input.previewLevel ?? pokeInBox.level;
       const calcOpts: GetPokemonProducingRateOpts = {
         berryData: berryDataMap[pokemon.berry.id],
         skillData: mainSkillMap[pokemon.skill],
         seeds: pokeInBox.seeds ?? defaultSeedUsage,
         pokemonProducingParams: getPokemonProducingParams({
-          pokemonId: pokemon.id,
+          pokemonId: id,
           pokemonProducingParamsMap,
         }),
         ingredientMap,
@@ -48,17 +72,19 @@ export const getTeamMakerRateAtMaxPotential = ({
         ...toCalculatedUserSettings({settings}),
         ...getProducingRateSingleParams({
           ...pokeInBox,
+          level,
           subSkillMap,
           helpingBonusSimulateOnSelf: true,
         }),
         // Override `level` because preview level might be active
-        level: input.previewLevel ?? pokeInBox.level,
+        level,
         // Override `pokemon` in `pokeInBox`
         pokemon,
         // Override `ingredients` in `pokeInBox`
         ingredients: getEffectiveIngredientProductions(pokeInBox),
         // Override helper count to maximum possible value to calculate max potential
         helperCount: teamMakerMaxMemberCount,
+        evolutionCount,
       };
       const rate = getPokemonProducingRateSingle({
         snorlaxFavorite: input.snorlaxFavorite,
